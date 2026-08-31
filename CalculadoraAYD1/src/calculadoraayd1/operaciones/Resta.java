b0bf2fa73479e0c32a65e5e7481d5c5ab57f122a
HerberthReyes07 <herberthreyes202230236@cunoc.edu.gt>
2026-08-31 01:04:51 -0600

    Combinación de commits

--- CalculadoraAYD1/src/calculadoraayd1/operaciones/Resta.java
+++ CalculadoraAYD1/src/calculadoraayd1/operaciones/Resta.java
@@ -2,16 +2,19 @@ package calculadoraayd1.operaciones;
 
 public class Resta implements Operacion {
 
+    // Implementación del método calcular para la operación de resta
     @Override
     public double calcular(double num1, double num2) {
         return num1 - num2;
     }
 
+    // Implementación del método getNombre para obtener el nombre de la operación
     @Override
     public String getNombre() {
         return "Resta";
     }
 
+    // Implementación del método getSimbolo para obtener el símbolo de la operación
     @Override
     public String getSimbolo() {
         return "-";
